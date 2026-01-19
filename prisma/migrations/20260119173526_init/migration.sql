3ebe962c67a7780b4299b93ad03efd8fcb7ea04b
JasielLeal <jasieloficial@hotmail.com>
2026-01-19 14:36:17 -0300

    test

---
+++ prisma/migrations/20260119173526_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isOnSale` on the `store_products` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "store_products" DROP COLUMN "isOnSale";
